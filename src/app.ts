dea47186b627b7b4a29c73414e519891e5061196
Bruno Ferreira Gomes <gomes.bruno201@gmail.com>
2022-01-05 04:07:14 -0300

    feat: adiciona rotas

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 // import libs
 import express from 'express';
 
+// import files
+import index from './routes/index';
+import users from './routes/users';
+
 const app: express.Application = express();
 const PORT = process.env.PORT || 5000;
+
+// routes
+app.use('/', index);
+app.use('/users', users);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
