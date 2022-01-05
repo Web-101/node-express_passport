a7708e4bbf17d796d62aa6722040663cfc650d0b
Bruno Ferreira Gomes <gomes.bruno201@gmail.com>
2022-01-05 06:29:50 -0300

    feat: inicializa o passport

--- src/app.ts
+++ src/app.ts
@@ -8,16 +8,20 @@ import path from "path";
 import mongoose from "mongoose";
 import flash from "connect-flash";
 import session from "express-session";
+import passport from "passport";
+
+// import files
+import index from "./routes/index";
+import users from "./routes/users";
+import localPassport from "./config/passport";
+
+localPassport(passport);
 
 // db config
 mongoose
   .connect(process.env.DB_URI as string)
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log(err));
-
-// import files
-import index from "./routes/index";
-import users from "./routes/users";
 
 const app: express.Application = express();
 const PORT = process.env.PORT || 5000;
@@ -39,6 +43,10 @@ app.use(
   })
 );
 
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 // connect flash
 app.use(flash());
 
@@ -46,6 +54,7 @@ app.use(flash());
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   next();
 });
 
---
+++ src/config/passport.ts
@@ -0,0 +1,44 @@
+// import libs
+import passport_local from 'passport-local';
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+// load user model
+import User from '../models/User';
+
+const LocalStrategy = passport_local.Strategy;
+
+export default function(passport: any) {
+  passport.use(
+    new LocalStrategy({ usernameField: 'email' }, (email: string, password: string, done: any) => {
+      // Match user
+      User.findOne({ email: email })
+        .then((user: any) => {
+          if (!user) {
+            return done(null, false, { message: 'That email is not registered' });
+          }
+
+          // Match password
+          bcrypt.compare(password, user.password, (err: any, isMatch: any) => {
+            if (err) throw err;
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, { message: 'Password incorrect' });
+            }
+          });
+        })
+        .catch((err: any) => console.log(err));
+    })
+  );
+
+  passport.serializeUser((user: any, done: any) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id: any, done: any) => {
+    User.findById(id, (err: any, user: any) => {
+      done(err, user);
+    });
+  });
+}
