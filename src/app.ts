0e4f08ed7895e990d3b28547fabf051e4b31a6f8
Bruno Ferreira Gomes <gomes.bruno201@gmail.com>
2022-01-05 04:08:31 -0300

    feat: inicializa a view engine

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
+// import dotenv
+import 'dotenv/config';
+
 // import libs
 import express from 'express';
+import expressLayouts from 'express-ejs-layouts';
+import path from 'path';
 
 // import files
 import index from './routes/index';
@@ -7,6 +12,14 @@ import users from './routes/users';
 
 const app: express.Application = express();
 const PORT = process.env.PORT || 5000;
+
+// set view engine
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+// body parser
+app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.use('/', index);
